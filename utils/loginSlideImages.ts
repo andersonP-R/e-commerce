0f10b22b09cddc6ab264e653c845f64d0db413e2
Anderson Rivera <anderzonrivera480@gmail.com>
2023-04-11 15:31:50 -0500

    login slide images added

--- utils/loginSlideImages.ts
+++ utils/loginSlideImages.ts
@@ -3,20 +3,24 @@ export const loginSlideImages = [
     id: 1,
     picture: "/images/clothes1.jpg",
     alt: "clothes1",
+    description: "Productos de calidad",
   },
   {
     id: 2,
     picture: "/images/clothes2.jpg",
     alt: "clothes2",
+    description: "Tiendas en toda la región",
   },
   {
     id: 3,
     picture: "/images/clothes3.jpg",
     alt: "clothes3",
+    description: "Garantía de 30 días",
   },
   {
     id: 4,
     picture: "/images/delivery.jpg",
     alt: "delivery",
+    description: "Entrega segura e inmediata",
   },
 ];
