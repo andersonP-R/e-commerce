0a17d673ad2fdf1103410494c1f13468992ad3f9
Anderson Rivera <anderzonrivera480@gmail.com>
2023-04-11 22:39:35 -0500

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -11,48 +11,50 @@ export async function middleware(req: NextRequest) {
 
   if (!session) {
     const url = req.nextUrl.clone();
+    console.log(url);
     url.pathname = `/auth/login`;
+    console.log(url);
     url.search = `p=${requestedPage}`;
 
-    if (requestedPage.includes("/api")) {
-      return new Response(JSON.stringify({ message: "No autorizado" }), {
-        status: 401,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
+    // if (requestedPage.includes("/api")) {
+    //   return new Response(JSON.stringify({ message: "No autorizado" }), {
+    //     status: 401,
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
+    // }
 
     return NextResponse.redirect(url);
   }
 
   if (
-    requestedPage.includes("/api/admin") &&
-    !validRoles.includes(session.user.role)
-  ) {
-    return new Response(JSON.stringify({ message: "No autorizado" }), {
-      status: 401,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
-
-  if (
-    requestedPage.includes("/admin") &&
+    requestedPage.startsWith("/admin") &&
     !validRoles.includes(session.user.role)
   ) {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
+  // if (
+  //   requestedPage.includes("/api/admin") &&
+  //   !validRoles.includes(session.user.role)
+  // ) {
+  //   return new Response(JSON.stringify({ message: "No autorizado" }), {
+  //     status: 401,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  // }
 
   return NextResponse.next();
 }
 
 export const config = {
   matcher: [
-    "/checkout/:path*",
-    "/api/orders/:path*",
     "/admin/:path*",
+    "/api/orders/:path*",
     "/api/admin/:path*",
+    "/checkout/:path*",
   ],
 };
