0c1f00f0f0b6afdcc374505fc3637951124ee5fe
Anderson Rivera <anderzonrivera480@gmail.com>
2023-04-12 15:53:20 -0500

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
@@ -8,30 +9,31 @@ export async function middleware(req: NextRequest) {
   });
   const requestedPage = req.nextUrl.pathname;
   const validRoles = ["admin", "super-user", "SEO"];
+  // const url = req.nextUrl.clone();
+  // url.pathname = `/auth/login`;
+  // url.search = `p=${requestedPage}`;
 
-  if (!session) {
-    const url = req.nextUrl.clone();
-    url.pathname = `/auth/login`;
-    url.search = `p=${requestedPage}`;
+  if (!session) return NextResponse.redirect(new URL("/", req.url));
 
-    // if (requestedPage.includes("/api")) {
-    //   return new Response(JSON.stringify({ message: "No autorizado" }), {
-    //     status: 401,
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   });
-    // }
-
-    return NextResponse.redirect(url);
-  }
-
-  if (
-    requestedPage.startsWith("/admin") &&
-    !validRoles.includes(session.user.role)
-  ) {
+  if (session.user.role !== "admin") {
     return NextResponse.redirect(new URL("/", req.url));
   }
+
+  // if (requestedPage.includes("/api")) {
+  //   return new Response(JSON.stringify({ message: "No autorizado" }), {
+  //     status: 401,
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  // }
+
+  // if (
+  //   requestedPage.startsWith("/admin") &&
+  //   !validRoles.includes(session.user.role)
+  // ) {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
 
   // if (
   //   requestedPage.includes("/api/admin") &&
@@ -49,5 +51,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: "/admin/:path*",
 };
